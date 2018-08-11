97ebd1882cdcda7251403985b7167fae1c9c89d8
Anthony Laibe <anthony@laibe.cc>
2018-08-11 17:43:18 +0100

    Update infra scripts

---
+++ infra/scripts/guard_solon_env.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ -z "$SOLON_ENV" && echo "SOLON_ENV is required, please set us before continuing" && exit 1;
--- infra/scripts/start.sh
+++ infra/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker-compose -f infra/docker-compose.yml up -d;
-docker-compose -f infra/docker-compose.yml logs > logs/${SOLON_ENV}.log;
+docker-compose -f infra/docker-compose.yml logs > infra/logs/${SOLON_ENV}.log;
