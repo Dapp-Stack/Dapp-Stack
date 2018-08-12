ed3ab637e2033dda5e6ea61d42f803affb514031
Anthony Laibe <anthony@laibe.cc>
2018-08-12 22:00:05 +0100

    adding chokidar

--- contracts/scripts/compile.js
+++ contracts/scripts/compile.js
@@ -1,14 +1,11 @@
 #!/usr/bin/env node
 
 const Docker = require('dockerode');
-const each = require('async/each');
-
 const contracts = require('../config');
 
 let docker = new Docker();
 
-const compile = function(contract, next) {
-  console.log(`[Contracts] Starting to compile ${contract.name}`);
+const dockerSolc = function(contract) {
   const command = [
     '-o',
     `/solidity/build/${contract.name}`,
@@ -20,19 +17,35 @@ const compile = function(contract, next) {
     '--overwrite',
     `/solidity/src/${contract.name}`];
   const options = { Binds: [`${__dirname}/../src:/solidity/src`, `${__dirname}/../build:/solidity/build`] };
-  docker.run('ethereum/solc:0.4.24', command, process.stdout, options).then(function(container) {
+  return docker.run('ethereum/solc:0.4.24', command, process.stdout, options).then(function(container) {
     return container.remove();
-  }).then(function() {
+  });
+};
+
+const compile = function(contract) {
+  console.log(`[Contracts] Starting to compile ${contract.name}`);
+  dockerSolc(contract).then(function() {
     console.log(`[Contracts] Finished to compile ${contract.name}`);
-    next();
   }).catch(function(err) {
     console.log(`[Contracts] Error while compiling ${contract.name}`);
     console.log(err);
   });
 };
 
-each(contracts, function(contract, next) {
-  compile(contract, next);
-}, function(){
-  console.log("[Contracts] All contract compiled");
-});
+const compileSingle = function(name) {
+  let contract = contracts.find(c => c.name === name);
+  compile(contract);
+};
+
+const compileAll = function() {
+  contracts.map((contract) => {
+    compile(contract);
+  });
+};
+
+compileAll();
+
+module.exports = {
+  compileAll,
+  compileSingle
+};
--- contracts/scripts/watch.js
+++ contracts/scripts/watch.js
@@ -1,4 +1,11 @@
 const chokidar = require('chokidar');
-chokidar.watch(`../src/**/*.sol`, { cwd: __dirname }).on('all', (event, path) => {
-  console.log(event, path);
-});
+const { compileSingle } = require('./compile');
+
+let watcher = chokidar.watch(`../src/**/*.sol`, { cwd: __dirname, ignoreInitial: true });
+
+const compileAndDeploy = function(path) {
+  compileSingle(path.replace("../src/", ""));
+};
+
+watcher.on('add', path => compileAndDeploy(path));
+watcher.on('change', path => compileAndDeploy(path));
