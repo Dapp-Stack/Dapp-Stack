03639622fd3cdb4b2a5b47aceb53390e8537a568
Anthony Laibe <anthony@laibe.cc>
2018-08-12 21:15:57 +0100

    Fix env

--- infra/scripts/guard_solon_env.sh
+++ infra/scripts/guard_solon_env.sh
@@ -5,3 +5,17 @@ then
   echo "SOLON_ENV is required, please set us before continuing"
   exit 1;
 fi
+
+if [ "$SOLON_ENV" == "private" ]
+then
+  echo "[INFRA] Running private node";
+elif [ "$SOLON_ENV" == "rinkeby" ] || [ "$SOLON_ENV" == "ropsten" ]
+then
+  echo "[INFRA] Warning: Running test node";
+elif [ "$SOLON_ENV" == "frontier" ]
+then
+  echo "[INFRA] Danger: Running main node";
+else
+  echo "[INFRA] Error: SOLON_ENV is incorrect, please use: private, rinkeby, ropsten or frontier";
+  exit 1;
+fi
