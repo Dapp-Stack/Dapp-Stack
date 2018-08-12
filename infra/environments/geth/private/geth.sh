76c3e03726009a4e4cb13aac9c7522e42d9c263e
Anthony Laibe <anthony@laibe.cc>
2018-08-12 20:34:59 +0100

    Fix geth.sh for private

--- infra/environments/geth/private/geth.sh
+++ infra/environments/geth/private/geth.sh
@@ -5,17 +5,21 @@ geth --datadir "/root/.ethereum/private" \
      --ws \
      --wsaddr "0.0.0.0" \
      --wsorigins "*" \
-     --wsport "8446" \
+     --wsport "8546" \
      --rpc \
      --rpcapi "db,personal,eth,net,web3" \
      --rpcaddr "0.0.0.0" \
      --rpcport "8545" \
      --rpccorsdomain '*' \
-     --nodiscover \
-     --maxpeers 0 \
-     --verbosity 5 \
+     --unlock 0,1,2,3 \
      --mine \
-     --unlock 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19 \
      --password /root/.ethereum/private/passwords \
+     --dev \
      init "/root/.ethereum/private/genesis.json"
+
+#--nodiscover \
+#     --maxpeers 1 \
+#     --verbosity 5 \
+#     --mine \
+#     --dev \
 
--- infra/scripts/reset.sh
+++ infra/scripts/reset.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf ./data/geth/*/geth
+rm -rf ./data/geth/*/geth.ipc
 rm -rf ./data/ipfs/*
 docker-compose rm -f
 docker-compose down --rmi all
