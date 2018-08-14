68c47d94fdbc8271e18317d5d4697101d9ba9f6d
Anthony Laibe <anthony@laibe.cc>
2018-08-14 20:55:03 +0100

    Create components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import {
+  Dropdown,
+  Menu,
+  Button,
+} from 'semantic-ui-react'
 
-const Header = () => (
-  <p>No Match</p>
+const Header = (props) => (
+  <Menu inverted size='massive'>
+    <Menu.Item name='home' />
+    <Menu.Item
+      name='messages'
+    />
+
+    <Menu.Menu position='right'>
+      <Dropdown item text='Language'>
+        <Dropdown.Menu>
+          <Dropdown.Item>English</Dropdown.Item>
+          <Dropdown.Item>Russian</Dropdown.Item>
+          <Dropdown.Item>Spanish</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Menu.Item>
+        <Button primary>Sign Up</Button>
+      </Menu.Item>
+    </Menu.Menu>
+  </Menu>
 );
 
 export default Header;
--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,148 +1,19 @@
 import React from "react";
 import {
   Container,
-  Divider,
-  Dropdown,
-  Grid,
   Header,
-  Image,
-  List,
-  Menu,
-  Segment,
-  Button
 } from 'semantic-ui-react'
+import MHeader from "./Header";
+import MFooter from "./Footer";
 
 const Layout = (props) => (
-  <div>
-    <Menu inverted size='massive'>
-      <Menu.Item name='home' />
-      <Menu.Item
-        name='messages'
-      />
-
-      <Menu.Menu position='right'>
-        <Dropdown item text='Language'>
-          <Dropdown.Menu>
-            <Dropdown.Item>English</Dropdown.Item>
-            <Dropdown.Item>Russian</Dropdown.Item>
-            <Dropdown.Item>Spanish</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-
-        <Menu.Item>
-          <Button primary>Sign Up</Button>
-        </Menu.Item>
-      </Menu.Menu>
-    </Menu>
-
-    <Sidebar.Pushable as={Segment}>
-      <Sidebar
-        as={Menu}
-        animation='overlay'
-        icon='labeled'
-        inverted
-        onHide={this.handleSidebarHide}
-        vertical
-        visible={visible}
-        width='thin'
-      >
-        <Menu.Item as='a'>
-          <Icon name='home' />
-          Home
-        </Menu.Item>
-        <Menu.Item as='a'>
-          <Icon name='gamepad' />
-          Games
-        </Menu.Item>
-        <Menu.Item as='a'>
-          <Icon name='camera' />
-          Channels
-        </Menu.Item>
-      </Sidebar>
-
-      <Sidebar.Pusher>
-        <Segment basic>
-          <Header as='h3'>Application Content</Header>
-          <Image src='/images/wireframe/paragraph.png' />
-        </Segment>
-      </Sidebar.Pusher>
-    </Sidebar.Pushable>
-
+  <React.Fragment>
+    <MHeader />
     <Container text style={{ marginTop: '7em' }}>
       <Header as='h1'>Semantic UI React Fixed Template</Header>
-      <p>This is a basic fixed menu template using fixed size containers.</p>
-      <p>
-        A text container is used for the main container, which is useful for single column layouts.
-      </p>
-
-      <Image src='/images/wireframe/media-paragraph.png' style={{ marginTop: '2em' }} />
-      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
     </Container>
-
-    <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
-      <Container textAlign='center'>
-        <Grid divided inverted stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Group 1' />
-              <List link inverted>
-                <List.Item as='a'>Link One</List.Item>
-                <List.Item as='a'>Link Two</List.Item>
-                <List.Item as='a'>Link Three</List.Item>
-                <List.Item as='a'>Link Four</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Group 2' />
-              <List link inverted>
-                <List.Item as='a'>Link One</List.Item>
-                <List.Item as='a'>Link Two</List.Item>
-                <List.Item as='a'>Link Three</List.Item>
-                <List.Item as='a'>Link Four</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Group 3' />
-              <List link inverted>
-                <List.Item as='a'>Link One</List.Item>
-                <List.Item as='a'>Link Two</List.Item>
-                <List.Item as='a'>Link Three</List.Item>
-                <List.Item as='a'>Link Four</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Footer Header' />
-              <p>
-                Extra space for a call to action inside the footer that could help re-engage users.
-              </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-
-        <Divider inverted section />
-        <Image centered size='mini' src='/logo.png' />
-        <List horizontal inverted divided link>
-          <List.Item as='a' href='#'>
-            Site Map
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Contact Us
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Terms and Conditions
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Privacy Policy
-          </List.Item>
-        </List>
-      </Container>
-    </Segment>
-  </div>
+    <MFooter />
+  </React.Fragment>
 );
 
 export default Layout;
