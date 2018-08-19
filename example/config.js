f66e7827ea8c5cc890ef533bca803822fa55d139
Anthony Laibe <anthony@laibe.cc>
2018-08-19 20:53:12 +0100

    new config

--- example/config.js
+++ example/config.js
@@ -1,5 +1,11 @@
 module.exports = {
+  // When true, solon will try to start an ipfs node
+  ipfs: true,
+
+  // When true, solon will try to start a blockchain node
+  blockchain: true,
+
   // providerUrl is the url of the node you want to connect to.
   // If null or empty, we will then start a local node
-  providerUrl: ""
+  blockchainUrl: ""
 };
