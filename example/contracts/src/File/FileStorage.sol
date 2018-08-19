3fddf5bde7c48dc962c067763b6540622a5c52b6
Anthony Laibe <anthony@laibe.cc>
2018-08-19 17:32:34 +0100

    Cleaning

--- example/contracts/src/File/FileStorage.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.24;
-
-import "../Lib/Owned.sol";
-
-contract FileStorage is Owned {
-  mapping (uint => hash) files;
-}
--- example/contracts/src/Token/ERC721/ERC721.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.24;
-
-import "./ERC165Basic.sol";
-import "./ERC165Enumerable.sol";
-import "./ERC165Metadata.sol";
-
-contract ERC721 is ERC721Basic, ERC721Enumerable, ERC721Metadata {
-}
