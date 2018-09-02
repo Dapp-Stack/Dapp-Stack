0dfce0a47a0d3d5911a36913bc77423d9d8e45a2
Anthony Laibe <anthony@laibe.cc>
2018-09-02 15:06:47 +0100

    Improve

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -2,32 +2,38 @@ import * as Docker from 'dockerode';
 import { Environment } from '@solon/environment';
 
 let docker = new Docker();
-const mapping = {
-  sol: 'solc',
+interface CompilerMapping {
+  [key: string]: (contractName: string, environment: Environment) => Promise<any>;
+}
+const compilersMapping: CompilerMapping = {
+  sol: function(contractName: string) {
+    const command = [
+      '-o',
+      `/solidity/build/${contractName}`,
+      '--allow-paths',
+      '/solidity/src',
+      '--optimize',
+      '--combined-json',
+      'abi,asm,ast,bin,bin-runtime,clone-bin,devdoc,interface,opcodes,srcmap,srcmap-runtime,userdoc',
+      '--overwrite',
+      `/solidity/src/${contractName}`,
+    ];
+    const options = { Binds: [`${__dirname}/../src:/solidity/src`, `${__dirname}/../build:/solidity/build`] };
+    return docker.run('ethereum/solc:0.4.24', command, process.stdout, options).then(function(container) {
+      return container.remove();
+    });
+  },
+  notFound: function() {
+    return new Promise();
+  }
 };
 
-const dockerSolc = function(contractName: string) {
-  const command = [
-    '-o',
-    `/solidity/build/${contractName}`,
-    '--allow-paths',
-    '/solidity/src',
-    '--optimize',
-    '--combined-json',
-    'abi,asm,ast,bin,bin-runtime,clone-bin,devdoc,interface,opcodes,srcmap,srcmap-runtime,userdoc',
-    '--overwrite',
-    `/solidity/src/${contractName}`,
-  ];
-  const options = { Binds: [`${__dirname}/../src:/solidity/src`, `${__dirname}/../build:/solidity/build`] };
-  return docker.run('ethereum/solc:0.4.24', command, process.stdout, options).then(function(container) {
-    return container.remove();
-  });
-};
-
-export function compile(contractName: string, compileOption): void {
+export function compile(contractName: string, environment: Environment): Promise<void> {
   console.log(`[Contracts] Starting to compile ${contractName}`);
 
-  return dockerSolc(contractName, compileOption)
+  const type: string = Object.keys(compilersMapping).find((t: string) => contractName.endsWith(t)) || 'notFound';
+
+  return compilersMapping[type](contractName, environment)
     .then(function() {
       console.log(`[Contracts] Finished to compile ${contractName}`);
     })
@@ -39,6 +45,6 @@ export function compile(contractName: string, compileOption): void {
 
 export function compileAll(environment: Environment): void {
   environment.deploy.contracts.forEach((contractName: string) => {
-    compile(contractName, environment.services.compile);
+    compile(contractName, environment);
   });
 }
