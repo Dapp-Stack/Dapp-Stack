dd1093c6b62b10897472c3a3d6823b733f82d060
Anthony Laibe <anthony@laibe.cc>
2018-09-10 17:48:11 +0100

    adding compile

--- packages/scripts/src/shared/compile.ts
+++ packages/scripts/src/shared/compile.ts
@@ -1,16 +1,19 @@
-import { compile } from '@solon/compiler';
+import * as compiler from '@solon/compiler';
 import { Environment } from '@solon/environment';
 import { Signale } from 'signale';
 
 export function compileAll(environment: Environment): void {
-  const compiler = new Signale({scope: 'Compiler'});
-
   environment.deploy.contracts.forEach(async (contractName: string) => {
-    compiler.await(`Starting to compile: ${contractName}`)
-    compile(contractName, environment).then(() =>(
-      compiler.success(`Successfully compiled: ${contractName}`)
-    )).catch((error) => (
-      compiler.error(`Error while compiling: ${contractName}: ${error}`)
-    ));
+    compile(contractName, environment)
   });
+}
+
+export function compile(contractName: string, environment: Environment): void {
+  const signale = new Signale({scope: 'Compiler'});
+
+  compiler.compile(contractName, environment).then(() =>(
+    signale.success(`Successfully compiled: ${contractName}`)
+  )).catch((error) => (
+    signale.error(`Error while compiling: ${contractName}: ${error}`)
+  ));
 }
