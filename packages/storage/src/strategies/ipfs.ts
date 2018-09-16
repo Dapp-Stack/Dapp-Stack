7b11519f30b521238a2a6b814aa0775f36226f77
Anthony Laibe <anthony@laibe.cc>
2018-09-16 11:00:31 +0100

    Pipe logs

--- packages/storage/src/strategies/ipfs.ts
+++ packages/storage/src/strategies/ipfs.ts
@@ -3,6 +3,7 @@ import { Signale } from 'signale';
 import { IStorageStrategy } from '../types';
 import * as path from 'path';
 import * as spawn from 'cross-spawn';
+import * as fs from 'fs-extra';
 import { ChildProcess } from 'child_process';
 
 export class Ipfs implements IStorageStrategy {
@@ -10,22 +11,32 @@ export class Ipfs implements IStorageStrategy {
   private signale: Signale;
   private binaryPath: string;
   private childProcess?: ChildProcess;
+  private logStream: fs.WriteStream;
 
   constructor(config: Storage, signale: Signale) {
     this.config = config;
     this.signale = signale;
     this.binaryPath = path.resolve(__dirname, '..', '..', '..', 'node_modules', '.bin', 'ipfs');
+
+    fs.ensureDirSync(path.join(process.cwd(), 'logs'));
+    this.logStream = fs.createWriteStream(path.join(process.cwd(), 'logs', 'ipfs.log'));
   }
 
-  init() {
+  private init = () => {
     spawn.sync(this.binaryPath, ['init']);
+  }
+
+  private daemon = () => {
+    this.childProcess = spawn(this.binaryPath, ['daemon'], { stdio: 'pipe' });
+    this.childProcess.stdout.pipe(this.logStream);
+    this.childProcess.stderr.pipe(this.logStream);
   }
 
   start = () => {
     this.signale.await('Starting ipfs...')
     return new Promise<boolean>(resolve => {
       this.init();
-      this.childProcess = spawn(this.binaryPath, ['daemon']);
+      this.daemon();
       this.signale.success('Ipfs is running')
       resolve(true);
     });
