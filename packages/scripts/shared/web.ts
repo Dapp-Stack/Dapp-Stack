7250e03f73019534c1160f07541e856101142d60
Anthony Laibe <anthony@laibe.cc>
2018-09-17 18:12:30 +0100

    update scripts

--- packages/scripts/shared/web.ts
+++ packages/scripts/shared/web.ts
@@ -6,10 +6,10 @@ export function startWeb() {
   const signale = new Signale({ scope: 'Web' });
   signale.await('Starting web...');
   const reactScriptsPath = path.resolve(__dirname, '..', '..', 'node_modules', '.bin', 'react-scripts');
-  const child = spawn('node', [reactScriptsPath, 'start']);
+  const child = spawn('node', [reactScriptsPath, 'start'], { stdio: 'pipe' });
 
   child.stdout.on('data', (data: Buffer) => {
-    signale.info(data.toString('utf-8'));
+    data.toString('utf-8').trim().split("\n").forEach(line => signale.info(line))
   });
 }
 
@@ -17,10 +17,10 @@ export function buildWeb() {
   const signale = new Signale({ scope: 'Web' });
   signale.await('Building web...');
   const reactScriptsPath = path.resolve(__dirname, '..', '..', 'node_modules', '.bin', 'react-scripts');
-  const child = spawn('node', [reactScriptsPath, 'build']);
+  const child = spawn('node', [reactScriptsPath, 'build'], { stdio: 'pipe' });
 
   child.stdout.on('data', (data: Buffer) => {
-    signale.info(data.toString('utf-8'));
+    data.toString('utf-8').trim().split("\n").forEach(line => signale.info(line))
   });
 }
 
@@ -28,5 +28,5 @@ export function eject() {
   const signale = new Signale({ scope: 'Web' });
   signale.await('Starting to eject...');
   const reactScriptsPath = path.resolve(__dirname, '..', '..', 'node_modules', '.bin', 'react-scripts');
-  const child = spawn('node', [reactScriptsPath, 'eject'], { stdio: [process.stdin, process.stdout, process.stderr] });
+  spawn.sync('node', [reactScriptsPath, 'eject'], { stdio: [process.stdin, process.stdout, process.stderr] });
 }
