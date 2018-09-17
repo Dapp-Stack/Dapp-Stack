dd64831fb4a916d92539ca98dcb39e9e51f67439
Anthony Laibe <anthony@laibe.cc>
2018-09-17 21:01:54 +0100

    update command

--- packages/scripts/build.ts
+++ packages/scripts/build.ts
@@ -3,3 +3,25 @@
 process.on('unhandledRejection', err => {
   throw err;
 });
+
+import * as blockchain from '@solon/blockchain';
+import * as storage from '@solon/storage';
+import * as compiler from '@solon/compiler';
+import { Deployer } from '@solon/deployer';
+
+import * as lifecycle from './shared/lifecycle';
+
+import { buildWeb } from './shared/web';
+
+const environment = lifecycle.before();
+
+async function buildAsync() {
+  await blockchain.start(environment.services.blockchain);
+  await storage.start(environment.services.storage);
+  await compiler.run(environment.compile);
+  await new Deployer(environment.deploy).run();
+  buildWeb();
+  lifecycle.stopAsync(environment);
+}
+
+buildAsync();
--- packages/scripts/clean.ts
+++ packages/scripts/clean.ts
@@ -9,20 +9,16 @@ import * as path from 'path';
 import { Signale } from 'signale';
 import { Structure } from '@solon/environment';
 
-import * as lifecycle from './shared/lifecycle';
-
-const environment = lifecycle.before();
-
-const solonDir = path.join(process.cwd(), '.solon');
-const webDir = path.join(process.cwd(), '.build');
+const gethDir = path.join(process.cwd(), '.geth');
+const webDir = path.join(process.cwd(), 'build');
 const contractDir = path.join(process.cwd(), Structure.contracts.build);
 
 const signale = new Signale({ scope: 'Cleaner' });
-signale.await('cleaner started');
+signale.await('Cleaner started');
 
-if (fs.existsSync(solonDir)) {
-  fs.removeSync(solonDir);
-  signale.success('.solon directory removed');
+if (fs.existsSync(gethDir)) {
+  fs.removeSync(gethDir);
+  signale.success('.geth directory removed');
 }
 
 if (fs.existsSync(webDir)) {
