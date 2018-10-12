6a5b8efe660042ad3fa8a211c63e6da2f1b0d4c4
Anthony Laibe <anthony@laibe.cc>
2018-10-12 19:49:41 +0100

    Fix build

--- packages/web/scripts/src/strategies/null.d.ts
+++
@@ -1,9 +0,0 @@
-import { IWebStrategy } from '../types';
-export declare class Null implements IWebStrategy {
-    start: () => void;
-    build: () => Promise<void>;
-    eject: () => void;
-    test: () => void;
-    stop: () => void;
-}
-//# sourceMappingURL=null.d.ts.map
