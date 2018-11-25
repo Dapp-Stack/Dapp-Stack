5b02cf3adc6ded58b79e23fccf247fbdbe4a4289
John Solon <john.solon12@gmail.com>
2018-11-25 21:00:12 +0000

    feat: remove not needed

--- packages/vue-cli-plugin/README.md
+++
@@ -1,3 +0,0 @@
-### Dapp Stack
-
-TODO
--- packages/vue-cli-plugin/src/generator.ts
+++
@@ -1,9 +0,0 @@
-module.exports = (api: any, options: any, rootOptions: any) => {
-  api.extendPackage({
-    scripts: {
-      das: 'dapp-stack-scripts'
-    }
-  })
-
-  api.render('../../../template')
-}
