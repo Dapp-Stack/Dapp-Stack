b2a754cddb5817442920f9401c78d77492b61367
John Solon <john.solon12@gmail.com>
2018-11-25 19:12:30 +0000

    feat: compile ejs template for CRA

--- packages/create-react-app/src/index.ts
+++ packages/create-react-app/src/index.ts
@@ -1,3 +1,4 @@
+import * as ejs from 'ejs'
 import * as spawn from 'cross-spawn'
 import * as fs from 'fs-extra'
 import { EOL } from 'os'
@@ -27,7 +28,7 @@ function installDependencies (isYarn: boolean) {
     args = ['install', '--save']
   }
 
-  args.push('web3', 'solium')
+  args.push('@dapp-stack/scripts', '@dapp-stack/test', '@dapp-stack/secrets', 'solium')
 
   const proc = spawn.sync(command, args, { stdio: 'ignore' })
   if (proc.status !== 0) {
@@ -41,17 +42,22 @@ function updatePackage (appPath: string) {
   appPackage.dependencies = appPackage.dependencies || {}
 
   appPackage.scripts = {
-    das: 'dapp-stack-scripts'
+    das: 'dapp-stack-scripts',
+    secrest: 'dapp-stack-secrets'
   }
 
   fs.writeFileSync(path.join(appPath, 'package.json'), JSON.stringify(appPackage, null, 2) + EOL)
 }
 
 function installTemplate (appPath: string) {
-  const ownPackageName = require(path.join(__dirname, '..', 'package.json')).name
-  const ownPath = path.join(appPath, 'node_modules', ownPackageName)
-  const templatePath = path.join(ownPath, '..', '..', 'template')
+  const templatePath = path.join(__dirname, '..', '..', 'template')
   fs.copySync(templatePath, appPath)
+
+  const filename = path.join(appPath, 'environments', 'local.js.ejs')
+  const local = fs.readFileSync(filename, 'utf-8')
+  const compiled = ejs.render(local, { webFramework: 'react' })
+  fs.writeFileSync(path.join(appPath, 'environments', 'local.js'), compiled)
+  fs.unlinkSync(filename)
 }
 
 function initReactScripts (
@@ -62,16 +68,18 @@ function initReactScripts (
   template: string
 ) {
   const reactScripts = new Signale({ interactive: true, scope: 'React Scripts' })
+  reactScripts.await('[%d/2] - Installing react scripts...', 1)
   const scriptsPath = path.resolve(process.cwd(), 'node_modules', 'react-scripts', 'scripts', 'init.js')
   const init = require(scriptsPath)
   mute()
   init(appPath, appName, verbose, originalDirectory, template)
   unmute()
+  reactScripts.success('[%d/2] - Success', 2)
 }
 
 function initDappScripts (appPath: string) {
   const dappScripts = new Signale({ interactive: true, scope: 'Dapp Scripts' })
-  dappScripts.await('[%d/2] - Installing...', 1)
+  dappScripts.await('[%d/2] - Installing dapp stack...', 1)
 
   const isYarn = fs.existsSync(path.join(appPath, 'yarn.lock'))
   installDependencies(isYarn)
