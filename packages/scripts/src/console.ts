d3e6ecc5656dda3e5555dae677365bbd81ca10f3
John Solon <john.solon12@gmail.com>
2018-12-19 22:16:10 +0000

    feat: make console command self aware of eth node
    Start a geth node if needed before starting the console

--- packages/scripts/src/console.ts
+++ packages/scripts/src/console.ts
@@ -1,9 +1,12 @@
 import * as ethereum from '@dapp-stack/ethereum'
+import * as lifecycle from './shared/lifecycle'
 import { globalError } from './shared/globalError'
 
-function consoleAsync() {
+async function consoleAsync() {
   try {
+    await ethereum.start()
     ethereum.console()
+    lifecycle.after()
   } catch (error) {
     globalError(error)
   }
