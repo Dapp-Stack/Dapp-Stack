dbd6024086cf6cb9a30a0fdbebf8ce73c4fd856b
John Solon <john.solon12@gmail.com>
2018-12-27 13:12:02 +0100

    feat: avoid debugger to crash in case of bad tx

--- packages/debugger/src/index.ts
+++ packages/debugger/src/index.ts
@@ -1,18 +1,29 @@
 import { artifacts } from '@dapp-stack/contract-utils'
 import * as repl from 'repl'
 import * as Debugger from 'truffle-debugger'
-import Web3 = require('web3')
 import * as parser from 'solidity-parser-antlr'
+import { Signale } from 'signale'
+import Web3 = require('web3')
 
 import { Logger } from './logger'
 import { Evaluator } from './evaluator'
 
+const signale = new Signale({ scope: 'Debugger' })
+
 export async function start(txHash: string) {
   const provider = new Web3('http://localhost:8545')
-  const bugger = await Debugger.forTx(txHash, {
-    contracts: getContracts(),
-    provider
-  })
+  let bugger
+  try {
+    bugger = await Debugger.forTx(txHash, {
+      contracts: getContracts(),
+      provider
+    })
+  } catch (error) {
+    signale.error(
+      'We could not start the debugger, make sure the transaction hash is correct.'
+    )
+    process.exit(1)
+  }
   const session = await bugger.connect()
   const logger = new Logger(session)
   const evaluator = new Evaluator(session, logger)
--- packages/scripts/src/debugger.ts
+++ packages/scripts/src/debugger.ts
@@ -1,4 +1,5 @@
 import * as debug from '@dapp-stack/debugger'
+import * as compiler from '@dapp-stack/compiler'
 import * as ethereum from '@dapp-stack/ethereum'
 import * as lifecycle from './shared/lifecycle'
 import { globalError } from './shared/globalError'
@@ -8,6 +9,7 @@ export async function start(txHash: string) {
 
   try {
     await ethereum.start()
+    await compiler.run()
     await debug.start(txHash)
   } catch (error) {
     globalError(error)
